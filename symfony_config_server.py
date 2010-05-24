143cb76651e2135d4ad6f054555ae4546fe92d4f
Max Gribov <max@neuropunks.org>
2010-05-24 17:50:15 -0400

    added daemonizer

--- symfony_config_server.py
+++ symfony_config_server.py
@@ -6,12 +6,15 @@ def usage():
 
 
 def symfony_get_config(yml_string, app='frontend'):
+    """Read either cached or raw YAML config files"""
     yml_string.strip(os.linesep)
     app.strip(os.linesep)
     stringy = app, yml_string
     return ''.join(stringy)
 
+
 def handler(clientsock,addr):
+    """Handle the connections, parse received requests and send results back"""
     # default symfony app
     app = 'frontend'
 
@@ -31,6 +34,7 @@ def handler(clientsock,addr):
 
 
 def listener(ip='localhost', port='21567'):
+    """Spawn listener threads"""
     HOST = ip
     PORT = int(port)
     ADDR = (HOST, PORT)
@@ -43,6 +47,56 @@ def listener(ip='localhost', port='21567'):
         print 'connected from:', addr
         thread.start_new_thread(handler, (clientsock, addr))
 
+
+def daemonize():
+    # http://code.activestate.com/recipes/278731-creating-a-daemon-the-python-way/
+    """Detach a process from the controlling terminal and run it in the background as a daemon"""
+
+    UMASK = 0
+    WORKDIR = "/"
+    MAXFD = 1024
+
+    if (hasattr(os, "devnull")):
+        REDIRECT_TO = os.devnull
+    else:
+        REDIRECT_TO = "/dev/null"
+
+    try:
+        pid = os.fork()
+    except OSError, e:
+        raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+    if (pid == 0):
+        os.setsid()
+        try:
+            pid = os.fork()
+        except OSError, e:
+            raise Exception, "%s [%d]" % (e.strerror, e.errno)
+
+        if (pid == 0):
+            os.chdir(WORKDIR)
+            os.umask(UMASK)
+        else:
+            os._exit(0)
+    else:
+        os._exit(0)
+
+    import resource      
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    if (maxfd == resource.RLIM_INFINITY):
+        maxfd = MAXFD
+  
+    for fd in range(0, maxfd):
+        try:
+            os.close(fd)
+        except OSError: 
+            pass
+
+    os.open(REDIRECT_TO, os.O_RDWR)  # standard input (0)
+    os.dup2(0, 1)            # standard output (1)
+    os.dup2(0, 2)            # standard error (2)
+    return(0)
+            
 
 def main():
     # affects logging
@@ -70,8 +124,12 @@ def main():
         else:
             assert False, "unknown option"
 
+    # fork
+    retCode = daemonize()
+
     # spawn listening server
     listener(ip, port)
+
 
 
 
